ab828f37fd98870aab614c48d38bcf82f25b7bc8
Tomonori Ueno <t.uepon@gmail.com>
2017-08-01 11:34:54 +0900

    Revert "npm init"
    This reverts commit c031ca4217764c615ee1bae068c0cbb954a3300d.

--- app.js
+++ app.js
@@ -1,44 +1,15 @@
-//Module Dependencies
 var express = require('express');
-var stylus = require('stylus');
-var nib = require('nib');
 
-//Set port number
-var portnumber = 3000;
-
-//Initialize express
 var app = express();
-console.log('Express has been initialized');
 
-function(str,path){
-	return stylus(str)
-	.set('filename', path)
-	.use(nib())
-}
-
-//Set Views Folder
-app.set('views',__dirname+'/views');
-
-//Initialize pug
-app.set('view engine', 'pug');
-console.log('pug has been initialized');
-
-//Stylus Middleware
-app.use(express.logger('dev'));
-app.use(stylus.middleware(
-	{
-		src:__dirname + '/public',
-		compile: compile
-	}
-))
-app.use(express.static(__dirname+'/public'));
-
-//Render Index Page
-app.get('/', function(req,res){
-	res.render('index',
-	{title: 'Welcome'}
-	);
-})
-
-app.listen(portnumber);
-console.log(Server is now running on port '+portnumber');
+app.get('/',function(req,res){
+	res.render('index.ejs',{title: 'Hello World'});
+});
+app.get('/about',function(req,res){
+	res.render('layout.ejs',{title: 'About Us',body: '<h1>About Us</h1>'});
+});
+app.get('/*',function(req,res){
+	res.status(404).render('error.ejs',{title:"Error!"});
+});
+console.log('server started');
+app.listen(3000);
